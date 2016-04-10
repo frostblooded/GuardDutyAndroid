112079212ad17b737c56a123ddefe57948c5f141
frostblooded <frostblooded@yahoo.com>
2016-04-10 20:36:50 +0300

    Initial commit

---
+++ app/src/main/java/net/mc21/attendancecheck/CompanyLogIn.java
@@ -0,0 +1,13 @@
+package net.mc21.attendancecheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_log_in);
+    }
+}
---
+++ app/src/main/java/net/mc21/attendancecheck/MainActivity.java
@@ -0,0 +1,21 @@
+package net.mc21.attendancecheck;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "AttendanceCheck";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        Intent i = new Intent(this, CompanyLogIn.class);
+        startActivity(i);
+    }
+}
