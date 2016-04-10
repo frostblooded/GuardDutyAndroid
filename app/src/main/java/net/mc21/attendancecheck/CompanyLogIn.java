4632c1ee502a02f932c94385f281e9b116ae4146
frostblooded <frostblooded@yahoo.com>
2016-04-10 22:10:03 +0300

    Make HTTP requests work

--- app/src/main/java/net/mc21/attendancecheck/CompanyLogIn.java
+++ app/src/main/java/net/mc21/attendancecheck/CompanyLogIn.java
@@ -2,6 +2,10 @@ package net.mc21.attendancecheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import net.mc21.connections.HTTPRequest;
 
 public class CompanyLogIn extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class CompanyLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_log_in);
+    }
+
+    public void onClick(View v) {
+        Log.i(MainActivity.TAG, "Making request...");
+        String response = "";
+
+        byte[] data = "".getBytes();
+        response = HTTPRequest.sendAsync(data, "https://raw.githubusercontent.com/HristiyanZahariev/AttendanceCheck-RailsApp/master/config/boot.rb", HTTPRequest.RequestType.GET);
+
+        Log.i(MainActivity.TAG, "Response: " + response);
     }
 }
---
+++ app/src/main/java/net/mc21/connections/HTTPRequest.java
@@ -0,0 +1,94 @@
+package net.mc21.connections;
+
+import android.util.Log;
+
+import net.mc21.attendancecheck.MainActivity;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPRequest {
+    public static String SERVER_IP = "https://91.139.243.106:3000/";
+
+    public enum RequestType{
+        GET,
+        POST
+    }
+
+    public static String sendAsync(final byte[] sentInfo, final String to, final RequestType requestType) {
+        final StringBuilder response = new StringBuilder();
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String s = send(sentInfo, to, requestType);
+                    response.append(s);
+                } catch (IOException e) {
+                    Log.i(MainActivity.TAG, "HTTP request error: " + e.toString());
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.i(MainActivity.TAG, "HTTP Request interrupted: " + e.toString());
+        }
+
+        return response.toString();
+    }
+
+    public static String send(byte[] sentInfo, String to, RequestType requestType) throws IOException {
+        URL url;
+        HttpURLConnection connection = null;
+
+        //Create connection
+        url = new URL(to);
+        connection = (HttpURLConnection)url.openConnection();
+
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+        // Send POST output.
+        if(requestType == RequestType.POST) {
+            connection.setRequestMethod(requestType.toString());
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Content-Length", "" +
+                    Integer.toString(sentInfo.length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setDoOutput(true);
+
+            DataOutputStream printout = new DataOutputStream(connection.getOutputStream());
+            printout.write(sentInfo);
+            printout.flush();
+            printout.close();
+        }
+
+        //Get Response
+        InputStream is = connection.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        StringBuilder response = new StringBuilder();
+
+        int data = isr.read();
+        while(data != -1) {
+            char current = (char) data;
+            data = isr.read();
+            response.append(current);
+        }
+
+        connection.disconnect();
+        return response.toString();
+    }
+}
