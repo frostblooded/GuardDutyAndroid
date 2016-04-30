817463ef457def54984a97b2e3032981fadc3c33
frostblooded <frostblooded@yahoo.com>
2016-04-30 10:53:34 +0300

    Rewrite 'log_in' to 'login' and 'sign_up' to 'signup'

--- app/src/main/java/net/mc21/attendancecheck/CompanyLogIn.java
+++ app/src/main/java/net/mc21/attendancecheck/CompanyLogIn.java
@@ -2,18 +2,14 @@ package net.mc21.attendancecheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class CompanyLogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company_log_in);
+        setContentView(R.layout.activity_company_login);
     }
 
     public void logInCompany(View v) {
--- app/src/main/java/net/mc21/attendancecheck/WorkerLogIn.java
+++ app/src/main/java/net/mc21/attendancecheck/WorkerLogIn.java
@@ -4,13 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -24,38 +22,57 @@ public class WorkerLogIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_worker_log_in);
+        setContentView(R.layout.activity_worker_login);
     }
 
-    private void sendGCMToken(String GCMToken) {
+    private void registerDevice() {
+
+    }
+
+    private void checkWorkerLogin() {
+
+    }
+
+    private void validateLogIn(String firstName, String lastName, String password) {
         JSONObject json = new JSONObject();
         try {
-            json.put("gcm_token", GCMToken);
+            json.put("first_name", firstName);
+            json.put("last_name", lastName);
+            json.put("password", password);
         } catch (JSONException e) {
             Log.i(MainActivity.TAG, "JSON put error: " + e.toString());
             e.printStackTrace();
         }
 
-        String url = MainActivity.SERVER_IP + "api/v1/mobile/register_device";
+        String url = MainActivity.SERVER_IP + "api/v1/mobile/check_worker_login";
 
-        // Request a string response
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, json,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.i(MainActivity.TAG, response.toString());
+                        boolean success = false;
+                        try {
+                            success = response.getBoolean("success");
+                            Log.i(MainActivity.TAG, String.valueOf(success));
+
+                            if(success) {
+                                registerDevice();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }, new CustomErrorListener());
 
-        jsonRequest.setRetryPolicy(new DefaultRetryPolicy(25, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        jsonRequest.setRetryPolicy(new DefaultRetryPolicy(5000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
 
-        // Add the request to the queue
         Volley.newRequestQueue(this).add(jsonRequest);
     }
 
     public void logInWorker(View v) {
-        EditText worker_name = (EditText)findViewById(R.id.worker_log_in_username_field);
-        EditText password = (EditText)findViewById(R.id.worker_log_in_password_field);
-        sendGCMToken("rkgerigpuwehFPWRHOIREOPGRAEOPGEARHIGWEARGrkgerigpuwehFPWRHOIREOPGRAEOPGEARHIGWEARGrkgerigpuwehFPWRHOIREOPGRAEOPGEARHIGWEARGrkgerigpuwehFPWRHOIREOPGRAEOP");
+        String first_name = ((TextView)findViewById(R.id.worker_login_first_name_field)).getText().toString();
+        String last_name = ((TextView)findViewById(R.id.worker_login_last_name_field)).getText().toString();
+        String password = ((TextView)findViewById(R.id.worker_login_password_field)).getText().toString();
+        validateLogIn(first_name, last_name, password);
     }
 }
