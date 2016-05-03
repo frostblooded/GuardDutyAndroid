e525fc901bdfaebae33351a9b2d1ed40c6bd64f3
frostblooded <frostblooded@yahoo.com>
2016-05-03 22:49:51 +0300

    Make company login work

--- app/src/main/java/net/mc21/attendancecheck/CompanyLoginActivity.java
+++ app/src/main/java/net/mc21/attendancecheck/CompanyLoginActivity.java
@@ -1,8 +1,19 @@
 package net.mc21.attendancecheck;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Response;
+
+import net.mc21.connections.HTTP;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CompanyLoginActivity extends AppCompatActivity {
 
@@ -12,6 +23,42 @@ public class CompanyLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_company_login);
     }
 
+    private void saveGCMToken(String token) {
+        SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("access_token", token);
+        editor.commit();
+    }
+
     public void loginCompany(View v) {
+        String company_name = ((EditText) findViewById(R.id.company_login_company_name_field)).getText().toString();
+        String password = ((EditText) findViewById(R.id.company_login_password_field)).getText().toString();
+
+        JSONObject json = new JSONObject();
+
+        try {
+            json.put("company_name", company_name);
+            json.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = HTTP.SERVER_IP + "api/v1/mobile/login_company";
+
+        HTTP.POST(url, json, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String token = "";
+
+                try {
+                    token = response.getString("access_token");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Log.i(MainActivity.TAG, "Company login response token: " + token);
+                saveGCMToken(token);
+            }
+        }, this);
     }
 }
