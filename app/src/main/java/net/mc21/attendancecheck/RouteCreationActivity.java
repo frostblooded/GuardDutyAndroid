47e86590fd1cd96f3af888d4f47987e8045a7c88
frostblooded <frostblooded@yahoo.com>
2016-05-13 23:29:48 +0300

    Make map center and request GPS permission on activity start

--- app/src/main/java/net/mc21/attendancecheck/RouteCreationActivity.java
+++ app/src/main/java/net/mc21/attendancecheck/RouteCreationActivity.java
@@ -1,7 +1,18 @@
 package net.mc21.attendancecheck;
 
+import android.*;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,36 +22,91 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class RouteCreationActivity extends FragmentActivity implements OnMapReadyCallback {
+    private final static int ACCESS_LOCATION_REQUEST_CODE = 19;
+    private final static String PERMISSION_1 = Manifest.permission.ACCESS_FINE_LOCATION;
 
+    private Activity activity;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity = this;
         setContentView(R.layout.activity_route_creation);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, PERMISSION_1)) {
+            MainActivity.showToast("GPS is required to get your location. Please allow it.", getApplicationContext());
+        }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        ActivityCompat.requestPermissions(activity, new String[]{PERMISSION_1}, ACCESS_LOCATION_REQUEST_CODE);
+    }
+
+    private void setLocationEnabled() {
+        boolean hasPermissions = ActivityCompat.checkSelfPermission(this, PERMISSION_1) == PackageManager.PERMISSION_GRANTED;
+
+        if (!hasPermissions) {
+            Log.i(MainActivity.TAG, "No permissions for location!");
+            return;
+        }
+
+        mMap.setMyLocationEnabled(true);
+    }
+
+    private void setCameraOnDevicePos() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+        boolean hasPermissions = ActivityCompat.checkSelfPermission(this, PERMISSION_1) == PackageManager.PERMISSION_GRANTED;
+
+        if (!hasPermissions) {
+            Log.i(MainActivity.TAG, "No permissions for location!");
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        LatLng devicePos = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(devicePos));
+    }
+
+    private void initMap() {
+        setLocationEnabled();
+        setCameraOnDevicePos();
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        boolean hasPermissions = ActivityCompat.checkSelfPermission(this, PERMISSION_1) == PackageManager.PERMISSION_GRANTED;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (!hasPermissions) {
+            requestPermission();
+            return;
+        }
+
+        initMap();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case ACCESS_LOCATION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    MainActivity.showToast("Permission granted!", getApplicationContext());
+                    initMap();
+                } else {
+                    MainActivity.showToast("The map can't find your location! Please enter the map again and allow it to use GPS.", getApplicationContext());
+                    finish();
+                }
+
+                break;
+        }
     }
 }
