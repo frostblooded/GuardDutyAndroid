15b6ce67682cb17c1fd4342490db6b76e0a66cca
Nikolay Danailov <frostblooded@yahoo.com>
2016-06-16 11:08:37 +0300

    Remove unused constants from SPManager

--- app/src/main/java/net/mc21/attendancecheck/SPManager.java
+++ app/src/main/java/net/mc21/attendancecheck/SPManager.java
@@ -10,9 +10,6 @@ public class SPManager {
     public final static String SP_COMPANY_NAME = "company_name";
     public final static String SP_SITE_ID = "site_id";
     public final static String SP_WORKER_ID = "worker_id";
-    public final static String SP_SHIFT_START = "shift_start";
-    public final static String SP_SHIFT_END = "shift_end";
-    public final static String SP_CALL_INTERVAL = "call_interval";
 
     public static String getString(String string, Context context) {
         SharedPreferences sp = context.getSharedPreferences(SP_NAME, context.MODE_PRIVATE);
