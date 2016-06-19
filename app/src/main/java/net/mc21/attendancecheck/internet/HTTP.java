416a91e98310671634138624f0c62cc5947b31d8
Nikolay Danailov <frostblooded@yahoo.com>
2016-06-19 13:46:32 +0300

    Remove useless HTTP(custom class) methods

--- app/src/main/java/net/mc21/attendancecheck/internet/HTTP.java
+++ app/src/main/java/net/mc21/attendancecheck/internet/HTTP.java
@@ -24,33 +24,6 @@ public class HTTP {
     public final static int REQUEST_TIMEOUT = 5000;
     public static String SERVER_IP = "https://hidden-shelf-43728.herokuapp.com/";
     //public static String SERVER_IP = "http://91.139.243.106:3000/";
-    private static RequestQueue requestQueue;
-
-    public static void requestObject(int method, String url, JSONObject sentData,
-                                      Response.Listener<JSONObject> listener, Response.ErrorListener errorListener, Context context) {
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(method, url, sentData,
-                listener, errorListener);
-        jsonRequest.setRetryPolicy(new DefaultRetryPolicy(REQUEST_TIMEOUT,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-
-        if(requestQueue == null)
-            requestQueue = Volley.newRequestQueue(context);
-
-        requestQueue.add(jsonRequest);
-    }
-
-    public static void requestArray(int method, String url, JSONArray sentData,
-                                      Response.Listener<JSONArray> listener, Response.ErrorListener errorListener, Context context) {
-        JsonArrayRequest jsonRequest = new JsonArrayRequest(method, url, sentData,
-                listener, errorListener);
-        jsonRequest.setRetryPolicy(new DefaultRetryPolicy(REQUEST_TIMEOUT,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-
-        if(requestQueue == null)
-            requestQueue = Volley.newRequestQueue(context);
-
-        requestQueue.add(jsonRequest);
-    }
 
     public static void handleError(VolleyError error, Context context) {
         try {
